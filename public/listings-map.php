0726e602e72771c70a79f3a33a10493ce3b06139
buzztarget <buzztarget@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-25 07:41:13 +0000

    Fix glitch for not working property type filter in map widget
    git-svn-id: https://plugins.svn.wordpress.org/buzztarget/trunk@1053879 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/listings-map.php
+++ public/listings-map.php
@@ -3,8 +3,9 @@ namespace BuzzTargetLive;
 
 $listings = get_option('repl_listings');
 $map_options = get_option('buzztarget_map_options');
+$filtered_listings = array();
 
-foreach ($listings as $key => $listing) {
+foreach ($listings as $listing) {
     $matched = true;
 
     if($propertyTypeFilter) {
@@ -13,17 +14,29 @@ foreach ($listings as $key => $listing) {
         }
     }
 
-    $listings[$key]['PropertyMapIcon'] = (isset($map_options['markers'][$listing['PropertyTypes'][0]])
+    if (!$matched)
+        continue; // it did not match... nothin' to do with it
+
+    $listing['PropertyMapIcon'] = (isset($map_options['markers'][$listing['PropertyTypes'][0]])
         && strlen($map_options['markers'][$listing['PropertyTypes'][0]]) > 0) ? $map_options['markers'][$listing['PropertyTypes'][0]] : $map_options['markers']['default'];
+
+    $filtered_listings[] = $listing;
 }
 
 
 $vars = array(
-    'listings' => $listings,
+    'listings' => $filtered_listings,
     'map_options' => $map_options,
     'width' => $widgetWidth,
     'height' => $widgetHeight
 );
+// free some memory
+unset($filtered_listings);
+unset($listings);
+unset($map_options);
+unset($widgetWidth);
+unset($widgetHeight);
 
 echo $this->twig->render('listings-map.twig', $vars);
+unset($vars);
 ?>
